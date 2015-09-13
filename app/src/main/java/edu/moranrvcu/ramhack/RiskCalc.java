0b7c3ccb4f310845dc93b0830ac829d5ebca9f09
Dalton Dabney <armitronsport@gmail.com>
2015-09-13 00:41:30 -0400

    changed a few things. the calcRisk needs some work

--- app/src/main/java/edu/moranrvcu/ramhack/RiskCalc.java
+++ app/src/main/java/edu/moranrvcu/ramhack/RiskCalc.java
@@ -7,12 +7,24 @@ public class RiskCalc {
 
     double riskScore;
     boolean isRisk;
+    int pointLimit;
+    int necval;
 
-    public double calcRisk(double riskScore) {
+
+    public double calcRisk(Customer c,Item i)
+    {
+       /* int recents = 0;
+        pointLimit = (int)(c.getBalance()*.25);   // The balance of the account times .25 to get the limit of the point
+        necval = i.getNecValue();                 // the necval is the priority of the item (education item or entertainment or living)
+        recents = (int) (c.totalRecentPurchaes()*.25);  // The sum of all the transactions in the recentPurchases array
+        */
         return 0;
     }
 
-    public boolean isRisk(double riskScore) {
+    public boolean isRisk(double riskScore)
+    {
         return false;
     }
+
+
 }
