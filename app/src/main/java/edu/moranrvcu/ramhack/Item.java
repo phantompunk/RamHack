e0a0ae05af0a9a1cb1e38fb5babf902c085b474a
phantompunk <rigo.moran0@gmail.com>
2015-09-12 21:24:07 -0400

    RiskCalc class created

--- app/src/main/java/edu/moranrvcu/ramhack/Item.java
+++ app/src/main/java/edu/moranrvcu/ramhack/Item.java
@@ -1,13 +1,15 @@
 package edu.moranrvcu.ramhack;
 
 /**
+ * Item class describes what variables are required for a item.
  * Created by rigo on 9/12/15.
  */
 public class Item {
-    String itemName = "";
-    String necType = "";
-    int necValue = 0;
-    double itemCost = 0;
+
+    String itemName;   // item name
+    String necType;    // type of item(ex eduacation, entertainment,living)
+    int necValue;      // value assigned to type of item
+    double itemCost;
 
     public String getItemName() {
         return itemName;
@@ -39,6 +41,13 @@ public class Item {
 
     public void setItemCost(double itemCost) {
         this.itemCost = itemCost;
+    }
+
+    public Item() {
+        this.itemName = "";
+        this.necType = "";
+        this.necValue = 0;
+        this.itemCost = 0;
     }
 
     public Item(String itemName, String necType, int necValue, double itemCost) {
---
+++ app/src/main/java/edu/moranrvcu/ramhack/RiskCalc.java
@@ -0,0 +1,18 @@
+package edu.moranrvcu.ramhack;
+
+/**
+ * Created by rigo on 9/12/15.
+ */
+public class RiskCalc {
+    
+    double riskScore;
+    boolean isRisk;
+
+    public double calcRisk(double riskScore) {
+        return 0;
+    }
+
+    public boolean isRisk(double riskScore) {
+        return false;
+    }
+}
